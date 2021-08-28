19e55983891dd4a9224d50d745fedf34a11bb610
Jack Purdon <jpur3846@uni.sydney.edu.au>
2021-08-28 12:42:52 +1000

    Created signup modal

--- yodal/src/App.js
+++ yodal/src/App.js
@@ -4,7 +4,7 @@ import NavBar from "./components/Navbar";
 import Footer from "./components/Footer";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Container from "react-bootstrap/Container";
-// import 'bootstrap/dist/css/bootstrap.min.css';
+import SignUpModal from "./components/SignUpModal";
 
 function App() {
   return (
@@ -14,13 +14,11 @@ function App() {
         <Router>
           <Switch>
             <Route path="/">
-              <p>Landing Page</p>
+              <SignUpModal />
             </Route>
             <Route path="/signup">
-              <p>Sign up</p>
             </Route>
             <Route path="/home">
-              <p>Home page</p>
             </Route>
           </Switch>
         </Router>
---
+++ yodal/src/components/SignUpModal/index.js
@@ -0,0 +1,58 @@
+import styled from "styled-components";
+import MainButton from '../buttons';
+import InputStyles from '../shared/InputStyles';
+
+const SignUpModalStyles = styled.div`
+    .modal-outer {
+        width: 43.6rem;
+        height: 40.8rem;
+        left: 381px;
+        top: 126px;
+        border-radius: 0.5rem;
+        display: flex;
+        flex-direction: column;
+        justify-content: flex-start;
+        align-items: flex-start;
+        padding: 30px,
+    }
+
+    .modal {
+        padding: 30px;
+        height: 40.8rem;
+    }
+
+    p, hr, input {
+        margin-bottom: 2.5rem;
+    }
+
+    h4 {
+        color: var(--primary);
+        font-weight: bold;
+    }
+
+    h5 {
+        margin-bottom: 1.2rem;
+    }
+`
+
+const SignUpModal = () => {
+    return (
+        <SignUpModalStyles>
+            <div className="modal-outer">
+                <div className="modal">
+                    <h4>SIGN UP</h4>
+                    <p>Already have an account? <a href="/">Log in.</a></p>
+                    <hr></hr>
+                    <h5>Email Address</h5>
+                    <InputStyles />
+
+                    <h5>Password</h5>
+                    <InputStyles />
+                    <MainButton>Create Account</MainButton>
+                </div>
+            </div>
+        </SignUpModalStyles>
+    )
+}
+
+export default SignUpModal;
