e5ae114ed688379be49e9a73c9e834609572fee5
Jack Purdon <jpur3846@uni.sydney.edu.au>
2021-08-28 10:51:40 +1000

    Added button styles

--- yodal/src/components/buttons/index.js
+++ yodal/src/components/buttons/index.js
@@ -1,19 +1,25 @@
+import { useState } from 'react';
 
-const MainButton = ({ style, text, onClick }) => {
+const MainButton = ({ style, text, onClick, textColor, buttonColor, disabled, height }) => {
+
+    const [hovered, setHovered] = useState(false);
 
     return (
         <button onClick={onClick}
             style={{
                 minWidth: '115px',
-                height: '41px',
-                background: '#134169',
+                height: height ? height : '41px',
+                background: hovered ? '#ACE3EF' : '#134169',
                 borderRadius: '8px',
                 border: '0px',
-                color: 'white',
+                color: textColor ? textColor : 'white',
+                fontWeight: 'bold',
                 ...style
             }}
+            onMouseEnter={() => setHovered(true)} 
+            onMouseLeave={() => setHovered(false)}
         >
-        {text}
+            {text}
         </button>
     )
 }
