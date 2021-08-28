f8c5935ed03d9afe1bcbc2a51fd43f67d081d3ed
zheneric <63391084+zheneric@users.noreply.github.com>
2021-08-28 19:17:24 +1000

    created post memo

--- yodal/src/components/shared/InputStyles.js
+++ yodal/src/components/shared/InputStyles.js
@@ -1,12 +1,12 @@
 import styled from "styled-components";
 
 const InputStyles = styled.input`
-    width: 356px;
-    height: 41px;
-    padding: 10px;
-    border: 2px solid #D9E0E7;
-    box-sizing: border-box;
-    border-radius: 6px;
-`
+  width: 356px;
+  height: 41px;
+  padding: 10px;
+  border: 2px solid #d9e0e7;
+  box-sizing: border-box;
+  border-radius: 6px;
+`;
 
 export default InputStyles;
